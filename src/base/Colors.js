d5f88fefc842f643431329caa88f82a2ab265a0d
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-03-24 15:22:56 +0200

    Create colors

---
+++ src/base/Colors.js
@@ -0,0 +1,13 @@
+const Colors = {
+  primary: '#0070E0',
+  secondary: '#06BEE1',
+  success: '#44AF69',
+  warning: '#FCAB10',
+  danger: '#F8333C',
+  black: '#1b2733',
+  grey: '#637282',
+  light: '#f5f5f5',
+  white: '#fff',
+};
+
+export default Colors;
