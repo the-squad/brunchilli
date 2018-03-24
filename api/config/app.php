0f91c1805625c54207d0ae7e9605d1a5342e6ac2
Muhammed Kamal <muhammedkmal3@gmail.com>
2018-03-24 12:09:02 +0200

    auto complete

--- api/config/app.php
+++ api/config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
