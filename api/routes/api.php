342d1c9192cc1a89afc366117931f1bd28a66074
Muhammed Kamal <muhammedkmal3@gmail.com>
2018-04-07 15:16:29 +0200

    enable cors

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,13 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::resource('/food','FoodController')->except(["create","edit"]);
-Route::resource('/category','FoodCategoryController')->except(["create","edit"]);
-Route::post('/login','UserController@Login');
-Route::post('/register','UserController@Register');
-Route::post('/food/comment','FoodController@createComment');
-Route::post('/food/updatecomment','FoodController@updateComment');
-Route::post('/food/deletecomment/','FoodController@deleteComment');
+Route::middleware(['cors'])->group(function (){
+    Route::resource('/food','FoodController')->except(["create","edit"]);
+    Route::resource('/category','FoodCategoryController')->except(["create","edit"]);
+    Route::post('/login','UserController@Login');
+    Route::post('/register','UserController@Register');
+    Route::post('/food/comment','FoodController@createComment');
+    Route::post('/food/updatecomment','FoodController@updateComment');
+    Route::post('/food/deletecomment/','FoodController@deleteComment');
+});
 
 
