4128f23e8ff187ca12a56016ab67347c3700e84f
Muhammed Kamal <muhammedkmal3@gmail.com>
2018-04-07 16:43:01 +0200

    add middleware cors to api

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware(['cors'])->group(function (){
+Route::group(['middleware' => 'cors'],function (){
     Route::resource('/food','FoodController')->except(["create","edit"]);
     Route::resource('/category','FoodCategoryController')->except(["create","edit"]);
     Route::post('/login','UserController@Login');
