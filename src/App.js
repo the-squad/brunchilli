c8d42379c795a0679df257da2bf0b468c8d12f69
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-04-07 20:40:48 +0200

    Init load user on mounting

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Header from './components/Header';
 import IconLoader from './components/IconLoader';
+import Splash from './components/Splash';
 
 import Home from './views/Home';
 import Search from './views/Search';
@@ -11,6 +12,7 @@ import Login from './views/Login';
 import SignUp from './views/SignUp';
 
 import icons from './selection.json';
+import User from './models/User';
 
 /**
  * Injecting global font family to all HTML nodes
@@ -27,24 +29,47 @@ class App extends Component {
   constructor(props) {
     super(props);
     IconLoader.getInstance().setIconStore(icons);
+
+    const user = new User();
+
+    this.state = {
+      isGettingUser: false, // TODO: replace with user.isUserExits()
+    };
+  }
+
+  componentDidMount() {
+    // TODO: get user data if the user exists
   }
 
   getHeaderRef = () => this.header;
 
   render() {
+    const { isGettingUser } = this.state;
+
+    if (isGettingUser) {
+      return <Splash />;
+    }
+
     return (
       <div className="App">
         <Header
           ref={header => {
             this.header = header;
           }}
+          {...this.props}
         />
 
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/results" component={Search} />
-          <Route path="/login" component={() => <Login getHeaderRef={this.getHeaderRef} />} />
-          <Route path="/register" component={() => <SignUp getHeaderRef={this.getHeaderRef} />} />
+          <Route
+            path="/login"
+            component={() => <Login {...this.props} getHeaderRef={this.getHeaderRef} />}
+          />
+          <Route
+            path="/register"
+            component={() => <SignUp {...this.props} getHeaderRef={this.getHeaderRef} />}
+          />
         </Switch>
       </div>
     );
