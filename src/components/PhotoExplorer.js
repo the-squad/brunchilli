31ae4ede0ed54689bde892f2e271e629fb5f4586
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-04-28 18:04:21 +0200

    Create PhotoExplorer component

---
+++ src/components/PhotoExplorer.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class PhotoExplorer extends Component {
+  onChange = () => {};
+
+  setProps = (onChange, callbackParams) => {
+    this.onChange = onChange;
+    this.callbackParams = callbackParams;
+  };
+
+  callbackParams;
+
+  uploadPhoto = () => {
+    const { files } = this.photoInput;
+    const uploadedFiles = [];
+    let loadedPhotos = -1;
+
+    Array.from(files).map(file => {
+      const picReader = new FileReader();
+      picReader.onloadend = () => {
+        uploadedFiles.push(picReader.result);
+        loadedPhotos += 1;
+        if (loadedPhotos === Array.from(files).length - 1) {
+          this.onChange(uploadedFiles, this.callbackParams);
+        }
+      };
+      picReader.readAsDataURL(file);
+    });
+  };
+
+  click = () => {
+    this.photoInput.click();
+  };
+
+  render() {
+    return (
+      <input
+        type="file"
+        multiple
+        onChange={this.uploadPhoto}
+        ref={photoInput => {
+          this.photoInput = photoInput;
+        }}
+        style={{ display: 'none' }}
+      />
+    );
+  }
+}
+
+export default PhotoExplorer;
