f46ec9f003ca9454f6003dc210171ba207691af1
Muhammed Kamal <muhammedkmal3@gmail.com>
2018-04-30 20:39:53 +0200

    solve array of array image

--- api/app/Http/Resources/PhotoResource.php
+++ api/app/Http/Resources/PhotoResource.php
@@ -9,13 +9,11 @@ class PhotoResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
-        return [
-            $this->getPath()
-        ];
+        return $this->getPath();
     }
 }
