5c30e42b0592288e3213dca6e13e69f421bd1ede
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-05-01 23:10:19 +0200

    Fix OrderRow columns

--- src/components/OrderRow.js
+++ src/components/OrderRow.js
@@ -12,7 +12,7 @@ import Colors from '../base/Colors';
 const OrderRowContainer = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: 5% 20% 20% 15% 25% 15%;
+  grid-template-columns: 7% 20% 21% 18% 25% 10%;
   cursor: ${props => !props.isHeader && 'pointer'};
   padding: 0 ${Spacing.get('4x')};
   height: 50px;
