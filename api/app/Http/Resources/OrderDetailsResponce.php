6c4fce6ea193cd5ac42379b04f5ff1024b770156
Muhammed Kamal <muhammedkmal3@gmail.com>
2018-05-01 18:10:31 +0200

    order list

---
+++ api/app/Http/Resources/OrderDetailsResponce.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderDetailsResponce extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "foodId" => $this->id,
+            "name" => $this->name,
+            "amount" => $this->pivot->quantity,
+            "price" => $this->price * $this->pivot->quantity,
+            "category" => $this->category ? $this->category->name : "",
+            "categoryId" => $this->category ? $this->category->id : "",
+        ];
+    }
+}
